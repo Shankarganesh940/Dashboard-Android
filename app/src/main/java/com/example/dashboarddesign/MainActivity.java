5a10bd3254a94e3ec5e74b119b14b2ca5fe36411
shankarganesh940 <shankarganesh940@gmail.com>
2019-06-25 15:57:12 +0530

    Initial Commit

--- app/src/main/java/com/example/dashboarddesign/MainActivity.java
+++ app/src/main/java/com/example/dashboarddesign/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.dashboarddesign;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    CardView actionfab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        actionfab = (CardView) findViewById(R.id.bankcardId);
+
+
+        actionfab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ComposeActivity.class));
+            }
+        });
+
+
     }
 }
